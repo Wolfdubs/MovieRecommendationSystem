2be5680f140d764c4f207100ab1eff4443ad4e7c
Wolfdubs <78827748+Wolfdubs@users.noreply.github.com>
2023-03-15 01:23:35 +0000

    recommendations methods with filters

--- MovieRecommendationSystemEfficientDatabases/MovieRunnerSimilarRatings.java
+++ MovieRecommendationSystemEfficientDatabases/MovieRunnerSimilarRatings.java
@@ -13,14 +13,16 @@ public class MovieRunnerSimilarRatings {
     private String ratingsFile = "ratings.csv";
     private String moviesFileShort = "ratedmovies_short.csv";
     private String ratingsFileShort = "ratings_short.csv";
+    private String ratingsFileQuiz = "ratingsQuiz.csv";
+    
     
     public void printAverageRatings(){
         FourthRatings fr = new FourthRatings();
-        RaterDatabase.initialize(ratingsFileShort);
+        RaterDatabase.initialize(ratingsFile);
         int uniqueRatersInDBCount = RaterDatabase.size();
         System.out.println("There are " + uniqueRatersInDBCount + " raters");
         
-        MovieDatabase.initialize(moviesFileShort);
+        MovieDatabase.initialize(moviesFile);
         int moviesInDBCount = MovieDatabase.size();
         System.out.println("There are " + moviesInDBCount + " movies");
         
@@ -31,7 +33,7 @@ public class MovieRunnerSimilarRatings {
             String ratingMovieTitle = MovieDatabase.getTitle(movieID);
             System.out.println(rating.getValue() + "\t" + ratingMovieTitle);
         }
-        System.out.println("Length of list of rated movies returned: " + ratings.size());
+        System.out.println("Length of list of rated movies returned: " + ratings.size() + "\n");
     }
     
     public void printAverageRatingsByYearAfterAndGenre(){
@@ -58,7 +60,7 @@ public class MovieRunnerSimilarRatings {
             String movieGenres = MovieDatabase.getGenres(movieID);
             System.out.println(rating.getValue() + " " + movieYear + " " + ratingMovieTitle + "\n\t" + movieGenres);
         }
-        System.out.println("Length of list of rated movies returned: " + filteredRatings.size());
+        System.out.println("Length of list of rated movies returned: " + filteredRatings.size() + "\n");
     }
     
     public void printSimilarRatings(){
@@ -71,7 +73,7 @@ public class MovieRunnerSimilarRatings {
         int moviesInDBCount = MovieDatabase.size();
         System.out.println("There are " + moviesInDBCount + " movies");
         
-        ArrayList<Rating> ratings = fr.getSimilarRatings("65",20,5) ;
+        ArrayList<Rating> ratings = fr.getSimilarRatings("71",20,5) ;
         //Collections.sort(ratings);
 
         int limitOfNumberOfRecommendations = 15;
@@ -80,13 +82,105 @@ public class MovieRunnerSimilarRatings {
             String ratingMovieTitle = MovieDatabase.getTitle(movieID);
             System.out.printf("%d %.2f %s \n", i+1, ratings.get(i).getValue(), ratingMovieTitle); 
         }
-        System.out.println("Length of list of rated movies returned: " + ratings.size());
+        System.out.println("Length of list of rated movies returned: " + ratings.size() + "\n");
     }
     
+    public void printSimilarRatingsByGenre(){
+        FourthRatings fr = new FourthRatings();
+        RaterDatabase.initialize(ratingsFile);
+        int uniqueRatersInDBCount = RaterDatabase.size();
+        System.out.println("There are " + uniqueRatersInDBCount + " raters");
+        
+        MovieDatabase.initialize(moviesFile);
+        int moviesInDBCount = MovieDatabase.size();
+        System.out.println("There are " + moviesInDBCount + " movies");
+        
+        Filter filter = new GenreFilter("Mystery");
+        ArrayList<Rating> ratings = fr.getSimilarRatingsByFilter("964",20,5,filter) ;
+        //Collections.sort(ratings);
 
+        int limitOfNumberOfRecommendations = 15;
+        for (int i = 0; i < Math.min(limitOfNumberOfRecommendations, ratings.size()); i++){
+            String movieID = ratings.get(i).getItem();
+            String ratingMovieTitle = MovieDatabase.getTitle(movieID);
+            System.out.printf("%d %.2f %s \n \t %s \n", i+1, ratings.get(i).getValue(), ratingMovieTitle, MovieDatabase.getGenres(movieID));
+        }
+        System.out.println("Length of list of rated movies returned: " + ratings.size() + "\n");
+    }
     
-    
+    public void printSimilarRatingsByDirector(){
+        FourthRatings fr = new FourthRatings();
+        RaterDatabase.initialize(ratingsFile);
+        int uniqueRatersInDBCount = RaterDatabase.size();
+        System.out.println("There are " + uniqueRatersInDBCount + " raters");
+        
+        MovieDatabase.initialize(moviesFile);
+        int moviesInDBCount = MovieDatabase.size();
+        System.out.println("There are " + moviesInDBCount + " movies");
+        
+        Filter filter = new DirectorsFilter("Clint Eastwood,J.J. Abrams,Alfred Hitchcock,Sydney Pollack,David Cronenberg,Oliver Stone,Mike Leigh");
+        ArrayList<Rating> ratings = fr.getSimilarRatingsByFilter("120",10,2,filter) ;
+        //Collections.sort(ratings);
 
+        int limitOfNumberOfRecommendations = 15;
+        for (int i = 0; i < Math.min(limitOfNumberOfRecommendations, ratings.size()); i++){
+            String movieID = ratings.get(i).getItem();
+            String ratingMovieTitle = MovieDatabase.getTitle(movieID);
+            System.out.printf("%d %.2f %s \n \t %s \n", i+1, ratings.get(i).getValue(), ratingMovieTitle, MovieDatabase.getDirector(movieID));
+            
+        }
+        System.out.println("Length of list of rated movies returned: " + ratings.size() + "\n");
+    }
+
+    public void printSimilarRatingsByGenreAndMinutes(){
+        FourthRatings fr = new FourthRatings();
+        RaterDatabase.initialize(ratingsFile);
+        int uniqueRatersInDBCount = RaterDatabase.size();
+        System.out.println("There are " + uniqueRatersInDBCount + " raters");
+        
+        MovieDatabase.initialize(moviesFile);
+        int moviesInDBCount = MovieDatabase.size();
+        System.out.println("There are " + moviesInDBCount + " movies");
+        
+        AllFilters allFilters = new AllFilters();
+        allFilters.addFilter(new GenreFilter("Drama"));
+        allFilters.addFilter(new MinutesFilter(80,160));
+        ArrayList<Rating> ratings = fr.getSimilarRatingsByFilter("168",10,3,allFilters) ;
+        //Collections.sort(ratings);
+
+        int limitOfNumberOfRecommendations = 15;
+        for (int i = 0; i < Math.min(limitOfNumberOfRecommendations, ratings.size()); i++){
+            String movieID = ratings.get(i).getItem();
+            String ratingMovieTitle = MovieDatabase.getTitle(movieID);
+            System.out.printf("%d %.2f %s %d" + "m" + "\n \t %s \n", i+1, ratings.get(i).getValue(), ratingMovieTitle, MovieDatabase.getMinutes(movieID),MovieDatabase.getGenres(movieID)); 
+        }
+        System.out.println("Length of list of rated movies returned: " + ratings.size() + "\n");
+    }
+    
+    public void printSimilarRatingsByYearAndMinutes(){
+        FourthRatings fr = new FourthRatings();
+        RaterDatabase.initialize(ratingsFile);
+        int uniqueRatersInDBCount = RaterDatabase.size();
+        System.out.println("There are " + uniqueRatersInDBCount + " raters");
+        
+        MovieDatabase.initialize(moviesFile);
+        int moviesInDBCount = MovieDatabase.size();
+        System.out.println("There are " + moviesInDBCount + " movies");
+        
+        AllFilters allFilters = new AllFilters();
+        allFilters.addFilter(new YearAfterFilter(1975));
+        allFilters.addFilter(new MinutesFilter(70,200));
+        ArrayList<Rating> ratings = fr.getSimilarRatingsByFilter("314",10,5,allFilters) ;
+        //Collections.sort(ratings);
+
+        int limitOfNumberOfRecommendations = 15;
+        for (int i = 0; i < Math.min(limitOfNumberOfRecommendations, ratings.size()); i++){
+            String movieID = ratings.get(i).getItem();
+            String ratingMovieTitle = MovieDatabase.getTitle(movieID);
+            System.out.printf("%d %.2f %s %d" + "m, year: " +  "%d \n", i+1, ratings.get(i).getValue(), ratingMovieTitle, MovieDatabase.getMinutes(movieID),MovieDatabase.getYear(movieID)); 
+        }
+        System.out.println("Length of list of rated movies returned: " + ratings.size() + "\n");
+    }
 }
 
 
